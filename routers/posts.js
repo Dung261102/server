de1d385fc2078026b2e5027b47647cf7f1ddf829
Dung261102 <20521211@gm.uit.edu.vn>
2023-12-18 18:32:47 +0700

    Update  routers/posts.js

--- routers/posts.js
+++ routers/posts.js
@@ -1,21 +1,23 @@
 import express from 'express';
-// import { getPosts, createPost, updatePost } from '../controllers/posts.js';
-import { getPosts } from '../controllers/posts.js';
+import { getPosts, createPost, updatePost } from '../controllers/posts.js';
+
 
 const router = express.Router();
 //http://localhost:5000/posts
 
 //Test
-router.get('/', (req, res) => {
-    res.send('router success')
-});
+// router.get('/', (req, res) => {
+//     res.send('router success')
+// });
 
 
 
 router.get('/', getPosts);
 
-// router.post('/', createPost);
 
-// router.post('/update', updatePost);
+//Khi sử dụng method post, thì controller post sẽ được thực thi
+router.post('/', createPost); //rout này sử dụng để lấy tất cả bài post, delete và update
+
+router.post('/update', updatePost);
 
 export default router;
